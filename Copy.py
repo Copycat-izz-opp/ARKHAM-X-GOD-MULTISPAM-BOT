26c3556c56bb54c45d81df8167d0cddcd55619d1
Copycat-izz-opp <85601169+Copycat-izz-opp@users.noreply.github.com>
2022-01-20 12:22:50 +0530

    Copy.py

--- Copy.py
+++ Copy.py
@@ -1758,7 +1758,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"𝗣𝗢𝗡𝗚!\n`{ms}` ms{ALIVE_NAME} ")
+        await event.edit(f"𝗣𝗢𝗡𝗚!\n`{ms}` ms\n{ALIVE_NAME} ")
 
 
 
